e08c7dcb9b908c88e29ab436378565be0b742a9b
Walheimat <krister.schuchardt@gmail.com>
2023-12-08 23:16:03 +0100

    refac(scripts): don't use shorthand for pacify

--- scripts/dinghy-pacify.el
+++ scripts/dinghy-pacify.el
@@ -19,46 +19,46 @@
   "Customize `dinghy-pacify'."
   :group 'dinghy-pacify)
 
-(defcustom dp-not-testable nil
+(defcustom dinghy-pacify-not-testable nil
   "Regex matching files that are currently not testable."
   :type 'regexp
   :group 'dinghy-pacify)
 
-(defun dp--get-state (state)
+(defun dinghy-pacify--get-state (state)
   "Get the STATE."
   (when (hash-table-p flymake--state)
     (gethash state flymake--state)))
 
-(defun dp--ready-p ()
+(defun dinghy-pacify--ready-p ()
   "Check if the report concluded."
-  (when-let ((b-state (dp--get-state 'elisp-flymake-byte-compile))
-             (d-state (dp--get-state 'elisp-flymake-checkdoc)))
+  (when-let ((b-state (dinghy-pacify--get-state 'elisp-flymake-byte-compile))
+             (d-state (dinghy-pacify--get-state 'elisp-flymake-checkdoc)))
 
     (and (flymake--state-reported-p b-state)
          (flymake--state-reported-p d-state))))
 
-(defun dp--get-diags ()
+(defun dinghy-pacify--get-diags ()
   "Get all all state dialogs."
-  (append (flymake--state-diags (dp--get-state 'elisp-flymake-byte-compile))
-          (flymake--state-diags (dp--get-state 'elisp-flymake-checkdoc))))
+  (append (flymake--state-diags (dinghy-pacify--get-state 'elisp-flymake-byte-compile))
+          (flymake--state-diags (dinghy-pacify--get-state 'elisp-flymake-checkdoc))))
 
-(defun dp--get-severity-diags (severity)
+(defun dinghy-pacify--get-severity-diags (severity)
   "Get all dialogs of SEVERITY."
-  (cl-loop for diag in (dp--get-diags)
+  (cl-loop for diag in (dinghy-pacify--get-diags)
            if (eq (flymake--severity (flymake-diagnostic-type diag)) severity)
-           collect (dp--get-info diag)))
+           collect (dinghy-pacify--get-info diag)))
 
-(defvar dp--infos nil)
-(defvar dp--debugs nil)
-(defvar dp--warnings nil)
-(defvar dp--errors nil)
+(defvar dinghy-pacify--infos nil)
+(defvar dinghy-pacify--debugs nil)
+(defvar dinghy-pacify--warnings nil)
+(defvar dinghy-pacify--errors nil)
 
-(defvar dp--info 0)
-(defvar dp--debug (warning-numeric-level :debug))
-(defvar dp--warning (warning-numeric-level :warning))
-(defvar dp--error (warning-numeric-level :error))
+(defvar dinghy-pacify--info 0)
+(defvar dinghy-pacify--debug (warning-numeric-level :debug))
+(defvar dinghy-pacify--warning (warning-numeric-level :warning))
+(defvar dinghy-pacify--error (warning-numeric-level :error))
 
-(defun dp--get-info (diag)
+(defun dinghy-pacify--get-info (diag)
   "Get the relevant info from DIAG."
   (let ((text (flymake--diag-text diag))
         (beg (flymake--diag-beg diag))
@@ -66,37 +66,37 @@
 
     (list :file (buffer-file-name locus) :line (line-number-at-pos beg) :text text)))
 
-(defconst dp--checkers '(elisp-flymake-byte-compile elisp-flymake-checkdoc))
+(defconst dinghy-pacify--checkers '(elisp-flymake-byte-compile elisp-flymake-checkdoc))
 
-(defun dp--collect (file)
+(defun dinghy-pacify--collect (file)
   "Collect reports for FILE."
   (let ((counter 0))
 
     (with-current-buffer (find-file-noselect file)
       (setq sentence-end-double-space nil)
-      (setq flymake-diagnostic-functions dp--checkers)
+      (setq flymake-diagnostic-functions dinghy-pacify--checkers)
 
       (flymake-mode)
       (flymake-start nil)
 
-      (while (and (not (dp--ready-p)) (< counter 100))
+      (while (and (not (dinghy-pacify--ready-p)) (< counter 100))
         (setq counter (1+ counter))
         (sit-for 0.05))
 
-      (unless (dp--ready-p)
+      (unless (dinghy-pacify--ready-p)
         (error "Waited for five seconds for check to complete"))
 
-      (let ((warnings (dp--get-severity-diags dp--warning))
-            (errors (dp--get-severity-diags dp--error))
-            (debugs (dp--get-severity-diags dp--debug))
-            (infos (dp--get-severity-diags dp--info)))
+      (let ((warnings (dinghy-pacify--get-severity-diags dinghy-pacify--warning))
+            (errors (dinghy-pacify--get-severity-diags dinghy-pacify--error))
+            (debugs (dinghy-pacify--get-severity-diags dinghy-pacify--debug))
+            (infos (dinghy-pacify--get-severity-diags dinghy-pacify--info)))
 
-        (setq dp--warnings (append dp--warnings warnings)
-              dp--errors (append dp--errors errors)
-              dp--debugs (append dp--debugs debugs)
-              dp--infos (append dp--infos infos))))))
+        (setq dinghy-pacify--warnings (append dinghy-pacify--warnings warnings)
+              dinghy-pacify--errors (append dinghy-pacify--errors errors)
+              dinghy-pacify--debugs (append dinghy-pacify--debugs debugs)
+              dinghy-pacify--infos (append dinghy-pacify--infos infos))))))
 
-(defun dp--format (info)
+(defun dinghy-pacify--format (info)
   "Format INFO for output."
   (let ((file (plist-get info :file))
         (line (plist-get info :line))
@@ -104,50 +104,46 @@
 
     (format "%s:%s: %s" file line text)))
 
-(defun dp-check--get-package-files ()
+(defun dinghy-pacify-check--get-package-files ()
   "Get all testable package files."
   (let* ((files (shell-command-to-string "cask files"))
          (trimmed (string-trim files))
          (names (split-string trimmed "\n")))
 
-    (if (stringp dp-not-testable)
+    (if (stringp dinghy-pacify-not-testable)
         (progn
-          (message "Filtering files matching %s" dp-not-testable)
+          (message "Filtering files matching %s" dinghy-pacify-not-testable)
           (seq-filter
-           (lambda (it) (not (string-match dp-not-testable it)))
+           (lambda (it) (not (string-match dinghy-pacify-not-testable it)))
            names))
       names)))
 
-(defun dp-check ()
+(defun dinghy-pacify-check ()
   "Check all package files."
   (message "Checking package files with `flymake'")
 
   (condition-case err
-      (dolist (it (dp-check--get-package-files))
-        (dp--collect it))
+      (dolist (it (dinghy-pacify-check--get-package-files))
+        (dinghy-pacify--collect it))
     (error
      (message "Failed to check all of the packages: %s" (error-message-string err))
      (kill-emacs 0)))
 
-  (let* ((severe (append dp--errors dp--warnings))
-         (other (append dp--debugs dp--infos)))
+  (let* ((severe (append dinghy-pacify--errors dinghy-pacify--warnings))
+         (other (append dinghy-pacify--debugs dinghy-pacify--infos)))
 
     (when other
       (message "Found %s note(s)" (length other))
       (dolist (it other)
-        (message (dp--format it))))
+        (message (dinghy-pacify--format it))))
 
     (when severe
       (message "Found %s error(s)" (length severe))
 
       (dolist (it severe)
-        (message (dp--format it)))
+        (message (dinghy-pacify--format it)))
       (kill-emacs 1))))
 
 (provide 'dinghy-pacify)
 
 ;;; dinghy-pacify.el ends here
-
-;; Local Variables:
-;; read-symbol-shorthands: (("dp-" . "dinghy-pacify-"))
-;; End:
